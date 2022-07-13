3fab0f85fdfff8db957f76090e0ce119a621f573
Margarita Kosareva <kosareva-r@ya.ru>
2022-07-13 10:10:52 +0300

    first commit

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,23 @@
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
+for (let i = 1; i < 10; i++) { 
+    let a = 'hole' + i;
+    let hole = document.getElementById(a);
+    //alert(a);
+    console.log(hole);
+    hole.onclick =  (() => {
+        if (hole.className.includes( 'hole_has-mole' )) {
+            dead.textContent++;} else {
+                lost.textContent++;
+            } 
+        if (dead.textContent == 10) {
+            alert('Победа!');
+            dead.textContent = 0;
+            lost.textContent = 0; } else 
+            if (lost.textContent == 5) {
+                alert('Вы проиграли.');
+                dead.textContent = 0;
+                lost.textContent = 0;
+            }
+     });
+  }
