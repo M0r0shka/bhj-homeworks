f53dbfa76263db0a85722af20b32940e44df0ad5
Margarita Kosareva <kosareva-r@ya.ru>
2022-07-13 10:16:05 +0300

    first commit

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -2,8 +2,9 @@ const timer = document.getElementById('timer')
 const count = setInterval(() => {
     timer.textContent--;
     if (timer.textContent == 0) {
-        alert("Вы победили в конкурсе!");
-        clearInterval(count);
+       alert("Вы победили в конкурсе!");
+       clearInterval(count);
       }
   }, 1000);
+
   
