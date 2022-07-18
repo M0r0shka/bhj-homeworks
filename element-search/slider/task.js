4bf9cac52267f10e6dc04106c2925330b801df40
Margarita Kosareva <kosareva-r@ya.ru>
2022-07-18 09:09:26 +0300

    second commit

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,6 +2,46 @@ let prev = document.getElementsByClassName('slider__arrow_prev');
 let next = document.getElementsByClassName('slider__arrow_next');
 let image = Array.from(document.getElementsByClassName('slider__item'));
 
+next.item(0).onclick = function () {
+    let itemActive = image.findIndex(item => {return item.className == 'slider__item slider__item_active';});
+    image[itemActive].className = 'slider__item';
+    (itemActive < image.length-1) ? itemActive++ : itemActive = 0;
+    image[itemActive].className = 'slider__item slider__item_active';
+} 
+
+prev.item(0).onclick = function () {
+    let itemActive = image.findIndex(item => {return item.className == 'slider__item slider__item_active';});
+    image[itemActive].className = 'slider__item';
+    ((itemActive > 0) && (itemActive < image.length +1) ) ? itemActive-- : itemActive = (image.length - 1);
+    image[itemActive].className = 'slider__item slider__item_active';
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function itemActive(item, idx) {
+    console.log(item.className);
+
+
+    return item.className == "slider__item slider__item_active";
+}
+let bf = image.findIndex(itemActive);
+console.log(bf);
+*/
+/*
 let count = 0;
 function sliderScroll() {
     count++;
@@ -40,3 +80,4 @@ prev.item(0).onclick = function () {
     }
 } 
 
+*/
