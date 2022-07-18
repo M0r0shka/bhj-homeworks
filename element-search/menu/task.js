37f7bf882f2f2c6e1c3983cba16b7a70dc7dea80
Margarita Kosareva <kosareva-r@ya.ru>
2022-07-18 14:11:42 +0300

    task.js modified

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,25 +1,12 @@
-let menu_link = Array.from(document.querySelectorAll('.menu__link'));
-let sub = document.querySelectorAll('.menu_sub');
-let count1 = 0, count2 = 0;
-menu_link.forEach((itm) => {
-    itm.onclick = (() => {
-        if (itm == menu_link[1]) {
-            count1++;
-            if ((count1 % 2) === 0) {
-                sub[0].className = 'menu menu_sub';
-            } else {
-                sub[0].className = 'menu menu_sub menu_active';
-                return false;
-            }
-        }
-        if (itm == menu_link[5]) {
-            count2++;
-            if (count2 % 2 === 0) {
-                sub[1].className = 'menu menu_sub';
-            } else {
-                sub[1].className = 'menu menu_sub menu_active';
-                return false;
-            }
-        }
+let sub = Array.from(document.querySelectorAll('.menu_sub'));
+sub.forEach((item) => {
+    let subOpen = item.closest('.menu__item').querySelector('a');
+    subOpen.onclick = (() => {
+        if (item.className == 'menu menu_sub') {
+            item.className = 'menu menu_sub menu_active';
+        } else {
+            item.className = 'menu menu_sub';
+        } 
+        return false;
     });
 });
